940ee330d0473ce25a606648425425c22031b33c
Angélica CarrMo <42524889+acarrmo@users.noreply.github.com>
2020-09-02 17:59:40 -0500

    Add files via upload
    Dibujar con el movimiento de teclas.

--- teclas/eventos.js
+++ teclas/eventos.js
@@ -6,24 +6,47 @@ var teclas =
   LEFT: 37
 };
 console.log(teclas);
-document.addEventListener("keyup",dibujarTeclado);
+document.addEventListener("keydown",dibujarTeclado);
+var cuadrito = document.getElementById("canvas");
+var papel = cuadrito.getContext("2d");
+var x = 150;
+var y = 150;
+
+dibujarLinea("red", 149, 149, 151, 151, papel);
+
+function dibujarLinea(color, x_inicial, y_inicial, x_final, y_final, lienzo)
+{
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
+  lienzo.lineWidth = 3;
+  lienzo.moveTo(x_inicial, y_inicial);
+  lienzo.lineTo(x_final, y_final);
+  lienzo.stroke();
+  lienzo.closePath();
+}
 
 function dibujarTeclado(evento)
 {
+  var colorcito = "blue";
+  var movimiento = 5;
   if (evento.keyCode == teclas.UP)
   {
-    console.log("Arriba")
+    dibujarLinea(colorcito,x,y,x, y-movimiento,papel);
+    y = y-movimiento;
   }
   if (evento.keyCode == teclas.DOWN)
   {
-    console.log("Abajo")
+    dibujarLinea(colorcito,x,y,x, y+movimiento,papel);
+    y = y+movimiento;
   }
   if (evento.keyCode == teclas.LEFT)
   {
-    console.log("Izquierda")
+    dibujarLinea(colorcito,x,y,x-movimiento,y,papel);
+    x = x-movimiento;
   }
   if (evento.keyCode == teclas.RIGHT)
   {
-    console.log("Derecha")
+    dibujarLinea(colorcito,x,y,x+movimiento,y,papel);
+    x = x+movimiento;
   }
 }
