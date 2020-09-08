708fabe885038e86d5d9eba5c21e7bc5d752a378
Angélica CarrMo <42524889+acarrmo@users.noreply.github.com>
2020-09-08 14:41:19 -0500

    Se agrega funcion esDivisible()

--- fizzbuzz/fizz.js
+++ fizzbuzz/fizz.js
@@ -1,20 +1,33 @@
 var numeros = 100;
 var divisible = false;
 
+function esDivisible(numero, divisor)
+{
+  if(numero % divisor == 0)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
 for(var i = 1; i <=100; i++)
 {
-  divisible = false;
-  if(i % 3 == 0)
+//  divisible = false;
+  if(esDivisible(i,3))
   {
     document.write("Fizz");
-    divisible = true;
+//    divisible = true;
   }
-  if(i % 5 == 0)
+  if(esDivisible(i,5))
   {
     document.write("Buzz");
-    divisible = true;
+//    divisible = true;
   }
-  if(i % 3 != 0 && i % 5 !=0)
+  if(!esDivisible(i,3) && !esDivisible(i,5))
+  //if(i%3 !=0 && i%5 !=0)
   //if(!divisible)
   {
     document.write(i);
