fd4a6e78b66c1359e9272abc0b121d2b192b7615
Angélica CarrMo <42524889+acarrmo@users.noreply.github.com>
2020-09-21 16:59:22 -0500

    Cajero
    Simula retirar dinero de un cajero y despliega las cantidades de los billetes entregados.

---
+++ cajero/cajero.js
@@ -0,0 +1,61 @@
+class billete
+{
+  constructor(v,c)
+  {
+    this.valor=v;
+    this.cantidad=c;
+  }
+}
+
+function EntregarDinero()
+{
+  var t = document.getElementById("dinero");
+  dinero = parseInt(t.value);
+  for(var bi of caja)
+  {
+    if(dinero > 0)
+    {
+      div = Math.floor(dinero / bi.valor);
+
+      if(div > bi.cantidad)
+      {
+        papeles = bi.cantidad;
+      }
+      else
+      {
+        papeles = div;
+      }
+      entregado.push(new billete(bi.valor, papeles));
+      dinero = dinero - (bi.valor * papeles);
+
+    }
+  }
+      if(dinero>0)
+      {
+        resultado.innerHTML = "No puedo darte esa cantidad :(";
+      }
+      else
+      {
+        for(var e of entregado)
+        {
+          if(e.cantidad > 0)
+          {
+             resultado.innerHTML += e.cantidad + " billetes de $" + e.valor + "<br />";
+          }
+        }
+      }
+}
+
+var caja = [];
+var entregado = [];
+caja.push(new billete(50,3));
+caja.push(new billete(20,2));
+caja.push(new billete(10,2));
+
+
+var dinero;
+var div = 0;
+var papeles = 0;
+var resultado = document.getElementById("resultado");
+var b = document.getElementById('Retirar');
+b.addEventListener("click",EntregarDinero);
